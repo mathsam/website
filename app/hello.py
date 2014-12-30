afb76c7a8aa0bec29aba4379803587916b5e778a
Junyi Chai <junyic@princeton.edu>
2014-12-29 23:40:11 -0500

    Add plot_tools

--- app/hello.py
+++ app/hello.py
@@ -8,6 +8,7 @@ from wtforms.validators import Required, NumberRange, ValidationError
 from main import tk_quote
 from main import black_scholes 
 from main.american_put_pricer import american_put
+from main.plot_tools import plot_line
 import numpy as np
 
 app = Flask(__name__)
@@ -56,16 +57,16 @@ class NameForm(Form):
         if value <= 0.0:
             raise ValidationError('Must be larger than 0')
 
-    volatility = StringField('Volatility',
+    volatility = StringField('Volatility (annualized)',
                          validators=[check_is_float]) 
-    expiration = StringField('Expiration',
+    expiration = StringField('Expiration (in year)',
                          validators=[check_is_float]) 
     spot = StringField('Spot price',
                          validators=[check_is_float]) 
     strike = StringField('Strike price',
                          validators=[check_is_float]) 
-    interest_rate = StringField('Interest rate',
-                         validators=[check_is_float]) 
+    interest_rate = StringField('Interest rate % (in percentage)',
+                         validators=[check_is_float], default='0.25') 
     submit = SubmitField('Submit')
 
 @app.route('/pricer', methods=['GET', 'POST'])
@@ -78,7 +79,7 @@ def pricer():
         expiration = float(form.expiration.data)
         spot       = float(form.spot.data)
         strike     = float(form.strike.data)
-        interest_rate= float(form.interest_rate.data)
+        interest_rate= float(form.interest_rate.data)/100.0
         session['name']  = call_or_put 
         spot_list  = np.linspace(spot/2,spot*1.5,10)
         if call_or_put[0] == 'p' and optype[0] == 'E':
@@ -94,6 +95,12 @@ def pricer():
                    interest_rate, volatility)) for it_spot in spot_list]
         session['value'] = str(round(value,2))
         session['spot_value_list']  = spot_value_list
+        spot_price_chart_html = plot_line(spot_list,
+                            [y for (tmp, y) in spot_value_list],
+                            'Spot vs. price for' + optype + call_or_put,
+                            'Spot',
+                            'Price')
+        session['chart_html'] = spot_price_chart_html
     else:
         session['value'] = None
         session['name']  = None 
@@ -102,7 +109,8 @@ def pricer():
     return render_template('pricer.html', form=form, 
                            ticket     = session.get('name'),
                            price      = session.get('value'),
-                           spot_value_list = session.get('spot_value_list')) 
+                           spot_value_list = session.get('spot_value_list'),
+                           spot_vs_price_plot = session.get('chart_html')) 
 
 
 if __name__ == '__main__':
