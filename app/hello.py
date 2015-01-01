985412106650583f33cc074c08876110caea1a72
Junyi Chai <junyic@princeton.edu>
2015-01-01 16:40:10 -0500

    change session from client-side to server side using simplekv enhance volatility tools: able to chart multiple tickets together with price

--- app/hello.py
+++ app/hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for, flash
+from flask import Flask, render_template, session, redirect, url_for, flash, request
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
@@ -8,21 +8,30 @@ from wtforms.validators import Required, NumberRange, ValidationError
 from main import tk_quote
 from main import black_scholes 
 from main.american_put_pricer import american_put
-from main.plot_tools import plot_line, plot_time_line
+from main.plot_tools import plot_line, plot_time_multilines
 import numpy as np
+import json
+#import redis
+#from simplekv.memory.redisstore import RedisStore
+from flask_kvsession import KVSessionExtension
+from simplekv.memory import DictStore
+import simplekv
 
 import sys
 sys.path.append('/media/junyic/Work/Courses/4th_year/SoftwareEngineering/Project/UltraFinance/')
 from data_analysis.volatility import get_hist_vol
 
+#store = RedisStore(redis.StrictRedis())
+store = DictStore()
+simplekv.TimeToLiveMixin = (20) # session expires after 60 sec
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'just a empty string'
+KVSessionExtension(store, app)
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -91,6 +100,7 @@ class PricerForm(Form):
 @app.route('/pricer', methods=['GET', 'POST'])
 def pricer():
     form = PricerForm()
+    print session.get('name')
     if form.validate_on_submit():
         call_or_put = form.call_or_put.data
         optype      = form.optype.data
@@ -99,7 +109,6 @@ def pricer():
         spot       = float(form.spot.data)
         strike     = float(form.strike.data)
         interest_rate= float(form.interest_rate.data)/100.0
-        session['name']  = call_or_put 
         spot_list  = np.linspace(spot/2,spot*1.5,10)
         if call_or_put[0] == 'p' and optype[0] == 'E':
             value = american_put(volatility, interest_rate,
@@ -113,6 +122,7 @@ def pricer():
                    it_spot, strike, expiration, 
                    interest_rate, volatility)) for it_spot in spot_list]
         session['value'] = str(round(value,2))
+        session['name']  = call_or_put 
         session['spot_value_list']  = spot_value_list
         spot_price_chart_html = plot_line(spot_list,
                             [y for (tmp, y) in spot_value_list],
@@ -121,6 +131,7 @@ def pricer():
                             'Spot',
                             'Price')
         session['chart_html'] = spot_price_chart_html
+#        return redirect(url_for('pricer'))
     else:
         session['value'] = None
         session['name']  = None 
@@ -137,33 +148,78 @@ class HisVolForm(Form):
     def check_if_ticket_exist(form, field):
         if field.data is None:
             raise ValidationError('This field is required')
+
+        if session.get('ylabels'):
+            ticket_set = json.loads(session.get('ylabels'))
+            if field.data.upper() in ticket_set:
+                raise ValidationError('Ticket already exists')
+            elif len(ticket_set) >= 5:
+                raise ValidationError('Total number of tickets must <= 5')
+
         try:
             ticket = field.data.upper()
-            vol, vol_date = get_hist_vol(ticket)
-            session['ticket'] = ticket 
-            form.vol = vol
-            form.vol_date = vol_date
+            vol, vol_date, adj_close = get_hist_vol(ticket)
+            form.vol = 100*vol
+            form.vol_date  = vol_date
+            form.adj_close = adj_close
         except ValueError:
             raise ValidationError('Sorry, ticket does not exist in database')
 
     ticket = StringField('Ticket (e.g., AAPL, FB, YHOO in S&P 500)',
                          validators=[check_if_ticket_exist])
-    submit = SubmitField('Submit')
+    submit = SubmitField('Add to chart')
 
 
 @app.route('/volatility', methods=['GET', 'POST'])
 def volatility():
+    session.permanent = False
     form = HisVolForm()
+    try:
+        if request.form['clear_chart'] == 'Clear chart':
+            session['tickets'] = None
+            session['ylabels'] = None
+            session['vol']     = None
+            session['adj_close'] = None
+            return render_template('volatility.html', form=form, 
+                           ticket = session.get('tickets'),
+                           vol_chart = session.get('vol_chart'),
+                           adj_chart = session.get('adj_chart'))
+    except KeyError:
+        pass
+
+    print session.get('tickets'), session.get('ylabels')
     if form.validate_on_submit():
-        session['ticket'] = form.ticket.data.upper()
-        session['vol_chart'] = plot_time_line(form.vol_date,
-                                              form.vol*100.0,
-                        'vol_chart','Historical volatility (%)','Date','HV %')
-    else:
-        session['ticket'] = None
+        if session.get('tickets') is None:
+            session['tickets'] = form.ticket.data.upper()
+        else:
+            session['tickets'] += ' ' + form.ticket.data.upper()
+
+        if session.get('ylabels') is None:
+            vol       = []
+            adj_close = []
+            ylabels   = []
+        else:
+            vol       = json.loads(session.get('vol'))
+            adj_close = json.loads(session.get('adj_close'))
+            ylabels   = json.loads(session.get('ylabels'))
+
+        vol.append(list(form.vol))
+        adj_close.append(list(form.adj_close))
+        ylabels.append(form.ticket.data.upper())
+        session['vol']       = json.dumps(vol)
+        session['adj_close'] = json.dumps(adj_close)
+        session['ylabels']   = json.dumps(ylabels)
+
+        session['vol_chart'] = plot_time_multilines(form.vol_date,
+                                vol, 'vol_chart', 'Date', ylabels)
+        session['adj_chart'] = plot_time_multilines(form.vol_date,
+                          adj_close, 'adj_chart', 'Date', ylabels)
+
+    print session.get('tickets'), session.get('ylabels')
     return render_template('volatility.html', form=form, 
-                           ticket     = session.get('ticket'),
-                           vol_chart  = session.get('vol_chart')) 
+                           ticket = session.get('tickets'),
+                           vol_chart = session.get('vol_chart'),
+                           adj_chart = session.get('adj_chart')) 
 
 if __name__ == '__main__':
     manager.run()
--- app/main/plot_tools.py
+++ app/main/plot_tools.py
@@ -1,4 +1,63 @@
-def plot_time_line(t, y, div_id, title='', xlabel='', ylabel=''):
+def plot_time_multilines(t, y, div_id, xlabel='', ylabel=None):
+    """
+    return a string which renders plot using time t as x axis and multiple lines
+    for web brower use
+    
+    INPUTS:
+        t: list of Timestamp objects
+        y: list of list/numpy array
+        div_id: string, unique
+        xlabel: string for the name of data in x-axis
+        ylabel: a list of strings for each list of data in y
+    """
+    if ylabel is None:
+        ylabel = [''] * len(y)
+
+    google_chart = u"""
+    <script type="text/javascript" src="https://www.google.com/jsapi?autoload={'modules':[{'name':'visualization','version':'1','packages':['annotationchart']}]}"></script>
+    <script type='text/javascript'>
+    google.load('visualization', '1', {'packages':['annotationchart']});
+    google.setOnLoadCallback(drawChart);
+    function drawChart() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('date', '%s');""" %xlabel
+
+    for column_name in ylabel:
+        google_chart += u"""data.addColumn('number', '%s');""" %column_name
+
+    google_chart += u"""
+    data.addRows([""" 
+
+    for (i, ti) in enumerate(t): 
+        google_chart += u"""[ new Date(%d, %d, %d)""" %(ti.year, ti.month-1, ti.day)
+        for yi in y:
+            google_chart += ", %s" %str(yi[i])
+        google_chart += u"""], 
+    """
+
+    google_chart += u"""
+    ]);
+
+    var chart = new google.visualization.AnnotationChart(document.getElementById('%s'));
+
+    var options = {
+      displayAnnotations: true,
+    };
+
+    chart.draw(data, options);
+    }
+    </script>
+    """ %div_id
+
+    google_chart += u"""
+    <div class="container-fluid">
+    <div id="%s"></div>
+    </div>""" %div_id
+
+    return google_chart
+
+
+def plot_time_line(t, y, div_id, xlabel='', ylabel=''):
     """
     return a string which renders time line plot for web broser
     
